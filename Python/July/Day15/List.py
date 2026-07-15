243afea40e5f5a5ea9110a84c8bc99b56a887c37
Vaishnavi Mali <malivaishnavi671@gmail.com>
2026-07-15 18:29:15 +0530

    Complete Task Day15 of Python and SQl

---
+++ Python/July/Day15/List.py
@@ -0,0 +1,35 @@
+list =["banana","kiwi","mango","apple","papaya","berry"]
+print(list)
+
+"""#indexing
+print(list[3])
+print(list[-2])
+
+#slicing
+print(list[2:4])
+
+#updating
+list[2]="swaberry"
+print(list)
+
+#deleting
+del list[2]
+print(list)"""
+
+#list methods
+
+list.append("coconut")
+print(list)
+
+list.sort()
+print(list)
+
+list.pop(2)
+print(list)
+
+list.reverse()
+print(list)
+
+list2=[1,2,3,4,5]
+list.extend(list2)
+print(list)
